75488f550bb9006d0cbe86aa11f377ecb09210a6
EvanBoyle <epb5te@virginia.edu>
2013-09-09 22:59:23 -0400

    serving a file

---
+++ answers.md
@@ -0,0 +1,14 @@
+Title: Problem Set 1 Answers
+Author: Evan Boyle
+//some of this from wikipedia
+1. User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36
+Mozille 5: compatible with mozilla rendering engine
+(X11; Linux i686): system details, direct x11, linux os i686 arch
+AppleWebKit/537.36: platform the browser uses
+(KHTML, like Gecko): browser platform details
+Chrome/29.0.1547.57: browser and version
+Safari/537.36: based on safari/version
+
+2. Rust aims to mitigate concurency bugs.  Changing a static variable could lead to problems in a multithreaded application, so this is considered unsafe.
+
+
--- zhttpto.rs
+++ zhttpto.rs
@@ -6,6 +6,7 @@
 // Version 0.1
 
 extern mod extra;
+use std::{os, uint, vec, io, result};
 
 use extra::uv;
 use extra::{net_ip, net_tcp};
@@ -14,8 +15,9 @@ use std::str;
 static BACKLOG: uint = 5;
 static PORT:    uint = 4414;
 static IPV4_LOOPBACK: &'static str = "127.0.0.1";
+static mut visitor_count : int =  0;
 
-fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::comm::SharedChan<Option<extra::net_tcp::TcpErrData>>)
+unsafe fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::comm::SharedChan<Option<extra::net_tcp::TcpErrData>>)
 {
     do spawn {
         let accept_result = extra::net_tcp::accept(new_conn);
@@ -32,20 +34,36 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
                     Err(err) => {
                         println(fmt!("Receive error: %?", err));
                     },
-                    Ok(bytes) => {
+                     Ok(bytes) => {
                         let request_str = str::from_bytes(bytes.slice(0, bytes.len() - 1));
                         println(fmt!("Request received:\n%s", request_str));
-                        let response: ~str = ~
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+                        let reqtype: ~[&str]= request_str.split_iter(' ').collect();
+                        inc_visitors();
+                        
+                        
+                        let response: ~str = match reqtype[1]{
+                        	"/"=>
+                            fmt!("HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                              <doctype !html><html><head><title>Hello, Rust!</title>
                              <style>body { background-color: #111; color: #FFEEAA }
                                     h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
                              </style></head>
                              <body>
                              <h1>Greetings, Rusty!</h1>
-                             </body></html>\r\n";
+                             <p>Number of Visitors: %d </p>
+                             </body></html>\r\n",  get_visitors()),
 
+                        
+                        	_=>"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"+serve_page("." + reqtype[1])
+                        
+                        
+                        
+                        };
+                        
                         net_tcp::write(&sock, response.as_bytes_with_null_consume());
+                        //let path = reqtype.split_iter(' ').advance().next();
+                         
+                         
                     },
                 };
             }
@@ -53,6 +71,26 @@ fn new_connection_callback(new_conn :net_tcp::TcpNewConnection, _killch: std::co
     };
 }
 
+fn serve_page(path: ~str)->~str{
+	
+	let fileReader: @Reader = io::file_reader(~PosixPath(path)).unwrap();
+	let mut bytes: ~[u8] = ~[];
+	loop{
+		let byte: int = fileReader.read_byte();
+		if fileReader.eof() {break}
+		bytes.push(byte as u8);
+	}
+	
+	return str::from_bytes(bytes);
+}
+
+unsafe fn inc_visitors(){
+	visitor_count+=1;
+	
+} 
+unsafe fn get_visitors()-> int{
+	visitor_count
+}
 fn main() {
     net_tcp::listen(net_ip::v4::parse_addr(IPV4_LOOPBACK), PORT, BACKLOG,
                     &uv::global_loop::get(),
